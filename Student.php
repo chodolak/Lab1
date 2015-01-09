fbc61519716420590eacfe9833d1a665abe6e555
Michael Chodolak <epicmaxzoo@gmail.com>
2015-01-09 08:51:45 -0800

    Added comments

--- Student.php
+++ Student.php
@@ -21,14 +21,17 @@ class Student {
         $this->grades = array();
     }
     
+    //Adds emails to student
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
     
+    //Adds grades to student
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    //Calculates average grades
     function average() {
         $total = 0;
         foreach($this->grades as $value)
@@ -36,6 +39,7 @@ class Student {
         return $total / count($this->grades);
     }
     
+    //Makes string of student's information
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
